9ad53b58782bddee986a9a1e63d2ac5c0dc7b713
RKH Technologies <rhysh6@gmail.com>
2020-07-14 22:45:04 +0100

    added in styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
-import './App.css';
+import styled from "styled-components";
+
+const PageContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          JF Fitness
-        </p>
-      </header>
-    </div>
+    <PageContainer>JF Fitness</PageContainer>
   );
 }
 
