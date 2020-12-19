03429b38447007aeec680e73c78ff465191f31b3
RKH Technologies <rhysh6@gmail.com>
2020-12-19 13:36:56 +0000

    Ebook page

--- src/Pages/E-Books/EBook.tsx
+++ src/Pages/E-Books/EBook.tsx
@@ -25,6 +25,7 @@ const Left = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
+  width: 50%;
 `;
 
 const Buy = styled.div`
@@ -69,7 +70,8 @@ const Image = styled.div`
   background-size: cover;
   background-position: center;
   border-radius: 10px;
-  width: 700px;
+  max-width: 700px;
+  width: 90%;
   height: 500px;
   margin: 0 50px;
   box-shadow: 0 2.8px 2.2px rgba(0,0,0,0.034), 0 6.7px 5.3px rgba(0,0,0,0.048), 0 12.5px 10px rgba(0,0,0,0.06), 0 22.3px 17.9px rgba(0,0,0,0.072), 0 41.8px 33.4px rgba(0,0,0,0.086), 0 100px 80px rgba(0,0,0,0.12);
